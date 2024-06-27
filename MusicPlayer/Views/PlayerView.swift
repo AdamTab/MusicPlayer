0e8f13f2a9d3e3afd573f3d0923ac0c5210dcad2
Adam <tabievadam@gmail.com>
2024-06-28 00:44:19 +0200

    2

---
+++ MusicPlayer/Views/PlayerView.swift
@@ -0,0 +1,20 @@
+//
+//  PlayerView.swift
+//  MusicPlayer
+//
+//  Created by Адам Табиев on 28.06.2024.
+//
+
+import SwiftUI
+
+struct PlayerView: View {
+    var body: some View {
+        ZStack {
+            BackgroundView()
+        }
+    }
+}
+
+#Preview {
+    PlayerView()
+}
